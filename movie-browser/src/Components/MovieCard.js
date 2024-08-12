ef2a43cc1e45a96ae4f1be089db126cd5ec06038
utkdev28 <gutkarsh1999@gmail.com>
2024-08-13 01:50:11 +0530

    "card height changes"

--- movie-browser/src/Components/MovieCard.js
+++ movie-browser/src/Components/MovieCard.js
@@ -8,7 +8,7 @@ export default function Card ({ title, content, imageUrl }){
       {imageUrl && <img src={`https://image.tmdb.org/t/p/w500/`+imageUrl} alt={title} className="card-image" />}
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
-        <p className="card-content">{content}</p>
+        <p className="card-content">{content.legth > 240 ? content.split('.')[0] : content }</p>
       </div>
     </div>
   );
